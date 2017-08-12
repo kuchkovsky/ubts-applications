aadb6c1078d10f85d902b447ff79a19949551acc
kuchkovsky <yaroslav.kuchkovsky@gmail.com>
2017-08-12 16:45:08 +0300

    Changed theme, added favicon

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -9,9 +9,25 @@
         ['ngMaterial', 'ngMessages', 'ngAnimate', 'lfNgMdFileInput', 'duScroll', 'ui.router']);
 
     app.config(function($mdThemingProvider) {
+        $mdThemingProvider.definePalette('ubtsBrownOrange', {
+            '50': 'fbe9e7',
+            '100': 'ffccbc',
+            '200': 'ffab91',
+            '300': 'ff8a65',
+            '400': 'ff7043',
+            '500': 'ff5722',
+            '600': 'f4511e',
+            '700': 'e64a19',
+            '800': 'bf5e3b',
+            '900': 'bf360c',
+            'A100': 'ff9e80',
+            'A200': 'ff6e40',
+            'A400': 'ff3d00',
+            'A700': 'dd2c00'
+        });
         $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('pink');
+            .primaryPalette('ubtsBrownOrange')
+            .accentPalette('red');
     });
 
     app.config(function($stateProvider, $urlRouterProvider) {
@@ -31,7 +47,7 @@
                 resolve: resolveDelay
             })
             .state('studentApplication', {
-                url: '/applications/student',
+                url: '/signup/student',
                 templateUrl : 'templates/student.html',
                 data : { pageTitle: 'Анкета абітурієнта УБТС' },
                 resolve: resolveDelay
