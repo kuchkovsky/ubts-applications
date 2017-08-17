91c3341dc16472e88d4dff1357e6d654d31c1869
kuchkovsky <yaroslav.kuchkovsky@gmail.com>
2017-08-17 23:05:02 +0300

    Added program fast search, increased size of card in student list

--- src/main/resources/static/js/student-list.js
+++ src/main/resources/static/js/student-list.js
@@ -4,21 +4,12 @@
 
     var app = angular.module('ubtsApplSystem');
 
-    app.service('downloadService', function ($http) {
-        this.getStudentList = function (onSuccess, onError) {
-            $http.get('/api/students/list').then(function (response) {
-                onSuccess(response.data);
-            }, function () {
-                onError();
-            });
-        };
-    });
-
     app.controller('studentListCtrl', function ($mdDialog, downloadService) {
         var self = this;
 
         this.students = [];
         this.form = {};
+        this.programMap = {};
 
         this.search = function () {
             if (!self.form.query) {
@@ -29,7 +20,8 @@
             var query = self.form.query.toLowerCase();
             self.students.forEach(function (student) {
                 if (student.name.toLowerCase().indexOf(query) !== -1
-                    || student.program.name.toLowerCase().indexOf(query) !== -1) {
+                    || student.program.name.toLowerCase().indexOf(query) !== -1
+                    || self.programMap[query] === student.program.name) {
                     self.form.students.push(student);
                 }
             });
@@ -40,6 +32,21 @@
             self.students = studentList;
             self.form.students = self.students.slice();
             self.isSpinnerVisible = false;
+            downloadService.getPrograms(function (programs) {
+                self.programs = programs;
+                self.programs.forEach(function (program) {
+                    var words = program.name.split(' ');
+                    var key = '';
+                    words.forEach(function (word) {
+                        key += word.substring(0, 1).toLowerCase();
+                    });
+                    self.programMap[key] = program.name;
+                });
+            }, function () {
+                var alert = $mdDialog.alert().title('Помилка')
+                    .textContent('Не вдалося завантажити список груп').ok('Закрити');
+                $mdDialog.show(alert);
+            });
         }, function () {
             var alert = $mdDialog.alert().title('Помилка').textContent('Не вдалося завантажити список').ok('Закрити');
             $mdDialog.show(alert);
