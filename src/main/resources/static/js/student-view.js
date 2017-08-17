02cc007db3ad6cc0e4eb6ef7c481b93cae868342
kuchkovsky <yaroslav.kuchkovsky@gmail.com>
2017-08-17 23:02:45 +0300

    View refactor

--- src/main/resources/static/js/student-view.js
+++ src/main/resources/static/js/student-view.js
@@ -4,11 +4,20 @@
 
     var app = angular.module('ubtsApplSystem');
 
-    app.controller('studentViewCtrl', function ($http, $stateParams) {
+    app.controller('studentViewCtrl', function ($rootScope, $stateParams, downloadService) {
         var self = this;
-        $http.get("/api/students/" + $stateParams.studentId).then(function (response) {
-            self.data = response.data;
+        $rootScope.studentId = $stateParams.studentId;
+        $rootScope.isMainSpinnerVisible = true;
+        downloadService.getStudent($stateParams.studentId, function (student) {
+            self.student = student;
+            $rootScope.isMainSpinnerVisible = false;
+        }, function () {
+            var alert = $mdDialog.alert().title('Помилка')
+                .textContent('Не вдалося завантажити дані абітурієнта').ok('Закрити');
+            $mdDialog.show(alert);
+            $rootScope.isMainSpinnerVisible = false;
         });
+
     });
 
 })();
